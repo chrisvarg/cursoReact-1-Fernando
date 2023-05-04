07a447bffbe8fd8b70b24db831bbea3d7694c35e
chrisvarg <cristianvargas385@gmail.com>
2023-05-03 22:29:00 -0500

    module 3, event click, useState

---
+++ 03-counter-app-vite/src/CounterApp2.jsx
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types";
+
+export const CounterApp2 = ({ value }) => {
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <h2>{value}</h2>
+    </>
+  );
+};
+
+CounterApp2.propTypes = {
+  value: PropTypes.number.isRequired,
+};
+
+CounterApp2.defaultProps = {
+  value: "Numero",
+};
